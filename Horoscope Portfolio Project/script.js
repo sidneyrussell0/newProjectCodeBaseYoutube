d8d9249dd720809380f382e2dbb571f12bbe4849
Sidney Russell <sidneyrussell58@gmail.com>
2024-09-14 16:42:27 -0700

    First Commit

---
+++ Horoscope Portfolio Project/script.js
@@ -0,0 +1,6 @@
+const horoscope1="Life is full of possibilities. Time to embrace them!"
+const horoscope2="Stars change, and so can we."
+const horoscope3="Look to the moon and stars to find the answers that lie within you."
+const hosocrope4="You should stay in today."
+
+
